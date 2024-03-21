1bd421cecd441788ff7a29dbf524ef3a8dada5b9
Yinka09 <kadirimaranatha@yahoo.com>
2024-03-21 19:58:59 +0100

    Update responsiveness

--- src/components/WeatherForecast.jsx
+++ src/components/WeatherForecast.jsx
@@ -5,13 +5,13 @@ import "../styles/WeatherForecast.css";
 export default function WeatherForecast() {
   return (
     <div className="weather-forecast container" id="weather-forecast">
-      <div className="row text-center text-white pt-5">
-        <div className="col-3 col-md-2">Mon</div>
-        <div className="col-3 col-md-2">Tues</div>
-        <div className="col-3 col-md-2">Wed</div>
-        <div className="col-3 col-md-2">Thur</div>
-        <div className="col d-none d-md-block">Fri</div>
-        <div className="col d-none d-md-block">Sat</div>
+      <div className="row text-center text-white pt-5 pt-md-5 pt-lg-3 weather-forecast-mid">
+        <div className="col-3 col-md-2 weather-forecast-date">Mon</div>
+        <div className="col-3 col-md-2 weather-forecast-date">Tues</div>
+        <div className="col-3 col-md-2 weather-forecast-date">Wed</div>
+        <div className="col-3 col-md-2 weather-forecast-date">Thur</div>
+        <div className="col d-none d-md-block weather-forecast-date">Fri</div>
+        <div className="col d-none d-md-block weather-forecast-date">Sat</div>
       </div>
       <div className="row text-center text-white  pt-4">
         <div className="col-3 col-md-2 fs-1">☀️</div>
